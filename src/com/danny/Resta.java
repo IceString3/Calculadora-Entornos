47b77aa2b7d5d04d7a7d8a65363740cd50eb2697
IceString3 <sepylo@outlook.com>
2020-05-25 13:55:15 +0100

    Refactored resta in Resta

--- src/com/danny/Resta.java
+++ src/com/danny/Resta.java
@@ -2,12 +2,12 @@ package com.danny;
 
 public class Resta extends Operacion{
 
-    private double resta;
+    private double subtract;
 
     public Resta(double n1, double n2) {
 
         super(n1, n2, '-');
-        this.resta = n1 - n2;
-        this.setRes(this.resta);
+        this.subtract = n1 - n2;
+        this.setRes(this.subtract);
     }
 }
